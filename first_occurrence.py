1f188519c660601b0cdff9d24905f3e55fdca249
Kves <vnlkbn@gmail.com>
2025-03-06 18:00:56 +0100

    first occurrence

---
+++ first_occurrence.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
+class Solution:
+    
+    def strStr(self, haystack, needle):
+        for i in range(len(haystack) - len(needle) + 1):
+            if haystack[i:i+len(needle)] == needle:
+                return i
+        return -1
