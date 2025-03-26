2f54c83777d49a1f9c0994acb6f2739fe12d3a7d
Kves <vnlkbn@gmail.com>
2025-03-26 18:13:33 +0100

    single number

---
+++ single_number.py
@@ -0,0 +1,10 @@
+#https://leetcode.com/problems/single-number/description/
+
+from typing import List
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        result = 0
+        for num in nums:
+            result ^= num
+        return result
